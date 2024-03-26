0690be4e0dedddb50428bf8c0da14672316698a5
BryanBorck <bryanborck@gmail.com>
2024-03-26 15:43:18 -0300

    starting frog

---
+++ frame/app/api/[[...routes]]/route.tsx
@@ -0,0 +1,308 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+// import { neynar } from 'frog/hubs'
+import { neynar } from 'frog/middlewares'
+import { handle } from 'frog/next'
+import { devtools } from 'frog/dev'
+import { serveStatic } from 'frog/serve-static'
+
+const neynarMiddleware = neynar({
+  apiKey: 'NEYNAR_FROG_FM',
+  features: ['interactor', 'cast'],
+})
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+}).frame('/', (c) => {
+  return c.res({
+    action: '/mintpage',
+    image: (
+      <div
+      style={{
+        alignItems: 'center',
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundImage: 'linear-gradient(to right, purple, blue)',
+        fontSize: 48,
+        height: '100%',
+        width: '100%',
+        padding: 20,
+      }}
+    >
+      Kismet Casa
+    </div>
+    ),
+    intents: [
+      <Button>Support</Button>,
+    ],
+  })
+}).frame('/mintpage', neynarMiddleware, (c) => {
+  const { displayName } = c.var.interactor || {}
+  return c.res({
+    action: '/finish',
+    image: (
+      <div
+      style={{
+        alignItems: 'center',
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundImage: 'linear-gradient(to right, purple, blue)',
+        fontSize: 48,
+        height: '100%',
+        width: '100%',
+        padding: 20,
+      }}
+    >
+      Hello {displayName || 'friend'}, mint your Kismet Casa NFT!
+    </div>
+    ),
+    intents: [
+      <Button>Skip</Button>,
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
+    ],
+  })
+}).frame('/finish', neynarMiddleware, (c) => {
+  const { transactionId } = c
+  const { followerCount } = c.var.interactor || {}
+  return c.res({
+    action: '/',
+    image: (
+      <div
+      style={{
+        alignItems: 'center',
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundImage: 'linear-gradient(to right, purple, blue)',
+        fontSize: 36,
+        height: '100%',
+        width: '100%',
+        padding: 20,
+      }}
+    >
+      TransactionId: ${transactionId || 'not realized'}
+      Thank you for the support, now share wit your {followerCount || 'many'} followers!
+    </div>
+    ),
+    intents: [
+      <Button>Support</Button>
+    ],
+  })
+}).transaction('/mint', (c) => {
+  return c.contract({
+    abi: wagmiExampleAbi,
+    chainId: 'eip155:84532',
+    functionName: 'mint',
+    to: '0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2',
+    args: [BigInt('123')],
+  })
+})
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
+
+const wagmiExampleAbi = [
+  { inputs: [], stateMutability: 'nonpayable', type: 'constructor' },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'approved',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'Approval',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'operator',
+        type: 'address',
+      },
+      { indexed: false, internalType: 'bool', name: 'approved', type: 'bool' },
+    ],
+    name: 'ApprovalForAll',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: 'address', name: 'from', type: 'address' },
+      { indexed: true, internalType: 'address', name: 'to', type: 'address' },
+      {
+        indexed: true,
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'Transfer',
+    type: 'event',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'to', type: 'address' },
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+    ],
+    name: 'approve',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    name: 'getApproved',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'owner', type: 'address' },
+      { internalType: 'address', name: 'operator', type: 'address' },
+    ],
+    name: 'isApprovedForAll',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'mint',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    name: 'mint',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    name: 'ownerOf',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'from', type: 'address' },
+      { internalType: 'address', name: 'to', type: 'address' },
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+    ],
+    name: 'safeTransferFrom',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'from', type: 'address' },
+      { internalType: 'address', name: 'to', type: 'address' },
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+      { internalType: 'bytes', name: '_data', type: 'bytes' },
+    ],
+    name: 'safeTransferFrom',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'operator', type: 'address' },
+      { internalType: 'bool', name: 'approved', type: 'bool' },
+    ],
+    name: 'setApprovalForAll',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'bytes4', name: 'interfaceId', type: 'bytes4' }],
+    name: 'supportsInterface',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'symbol',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    name: 'tokenURI',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'pure',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'from', type: 'address' },
+      { internalType: 'address', name: 'to', type: 'address' },
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+    ],
+    name: 'transferFrom',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+] as const
